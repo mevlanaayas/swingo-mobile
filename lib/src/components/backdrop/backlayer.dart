595846cc132c12845c02f64f0137f99f138f01c5
mevlanaayas <mevlanaayas@gmail.com>
2019-05-18 21:40:07 +0300

    apbar styling

--- lib/src/components/backdrop/backlayer.dart
+++ lib/src/components/backdrop/backlayer.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:meta/meta.dart';
 import 'package:swingo/src/models/product.dart';
+import 'package:swingo/src/pages/pages.dart';
+import 'package:swingo/src/theme/colors.dart';
 import 'package:swingo/src/utils/constans.dart';
 
 import 'frontlayer.dart';
 
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
-  final Widget frontLayer;
+  Widget frontLayer;
   final Widget backLayer;
   final Widget frontTitle;
   final Widget backTitle;
   final Widget subheader;
 
-  const Backdrop(
+  Backdrop(
       {@required this.currentCategory,
       @required this.frontLayer,
       @required this.backLayer,
@@ -30,11 +33,11 @@ class Backdrop extends StatefulWidget {
   _BackdropState createState() => _BackdropState();
 }
 
-class _BackdropState extends State<Backdrop>
-    with SingleTickerProviderStateMixin {
+class _BackdropState extends State<Backdrop> with TickerProviderStateMixin {
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
 
   AnimationController _controller;
+  int general = 0;
 
   @override
   void initState() {
@@ -105,6 +108,34 @@ class _BackdropState extends State<Backdrop>
 
   @override
   Widget build(BuildContext context) {
+    TabController _tabController =
+        TabController(initialIndex: general, length: 3, vsync: this);
+
+    void _handleTabSelection() {
+      if (_tabController.indexIsChanging) {
+        print("tab");
+        print(_tabController.index);
+        setState(() {
+          general = _tabController.index;
+          if (_tabController.index == 0) {
+            widget.frontLayer = HomePage();
+          } else if (_tabController.index == 1) {
+            widget.frontLayer = LoginPage();
+          } else {
+            widget.frontLayer = RegisterPage();
+          }
+        });
+      }
+    }
+
+    _tabController.addListener(_handleTabSelection);
+
+    @override
+    void dispose() {
+      _tabController.dispose();
+      super.dispose();
+    }
+
     var appBar = AppBar(
         brightness: Brightness.light,
         elevation: 0.0,
@@ -115,6 +146,11 @@ class _BackdropState extends State<Backdrop>
         ),
         title: Text('Swingo'),
         bottom: TabBar(
+          unselectedLabelColor: Colors.deepPurple.withOpacity(0.6),
+          indicator: BoxDecoration(
+              border: Border.all(color: altDeepPurple, width: 3),
+              borderRadius: BorderRadius.all(Radius.circular(40.0))),
+          controller: _tabController,
           tabs: [
             Tab(icon: Icon(Icons.directions_car)),
             Tab(icon: Icon(Icons.directions_transit)),
