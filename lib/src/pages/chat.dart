7e8bad18fcd4f4db4d4d4b380c5ef44f06f3f4b3
Furkan <furkan.akan@bitaksi.com>
2019-05-26 12:50:38 +0300

    Design of list page is changed and client is routed if clicked make a bid button

---
+++ lib/src/pages/chat.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
