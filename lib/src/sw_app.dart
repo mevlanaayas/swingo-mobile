f3770e2273c5842522e81e004d7e87be709258e9
mevlanaayas <mevlanaayas@gmail.com>
2019-05-29 20:24:04 +0300

    huh

--- lib/src/sw_app.dart
+++ lib/src/sw_app.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:swingo/src/models/filter.dart';
 import 'package:swingo/src/pages/pages.dart';
-import 'package:swingo/src/pages/welcome/about.dart';
 import 'package:swingo/src/pages/welcome/welcome.dart';
 
 class SwApp extends StatefulWidget {
@@ -12,8 +10,6 @@ class SwApp extends StatefulWidget {
 }
 
 class _SwAppState extends State<SwApp> {
-  Filter _currentFilter = Filter(fromCity: null, toCity: null, fromDate: null, toDate: null);
-
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
@@ -24,33 +20,8 @@ class _SwAppState extends State<SwApp> {
         '/': (context) => WelcomeScreen(),
         '/signin': (context) => SignInScreen(),
         '/signup': (context) => SignUpScreen(),
-        '/home': (context) => AboutScreen()
+        '/home': (context) => HomePage()
       },
     );
   }
-
-  void _onFilterChange(Filter filter){
-    setState(() {
-      _currentFilter = filter;
-      print('Filter state: $_currentFilter');
-    });
-  }
 }
-/*
-*       home: Backdrop(
-        currentFilter: _currentFilter,
-        //frontLayer: HomePage(filter: _currentFilter), // TODO: alttaki array bunun içine yazılmalı
-        frontLayer: <FrontlayerPage>[
-          FrontlayerPage(title: 'Send', child: ListPage()),
-          FrontlayerPage(title: 'Carry', child: ListPage()),
-        ],
-        backLayer: FilterMenuPage(
-          currentFilter: _currentFilter,
-          onFilterChange: _onFilterChange,
-        ),
-        frontTitle: Text('SHRINE'),
-        backTitle: Text('MENU'),
-        navbar: NavBar(),
-      ),
-      theme: swTheme,
-* */
