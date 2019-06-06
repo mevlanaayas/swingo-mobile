afdf21ea1120b809dfbd49de8aaecd3fde7e36e3
mevlanaayas <mevlanaayas@gmail.com>
2019-06-06 22:19:36 +0300

    detail page

--- lib/src/pages/bid_details.dart
+++ lib/src/pages/bid_details.dart
@@ -8,6 +8,72 @@ class DetailScreen extends StatelessWidget {
 
   DetailScreen({Key key, @required this.item}) : super(key: key);
 
+  Widget _buildHeading() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            const Icon(
+              FontAwesomeIcons.user,
+              color: secondaryColor,
+              size: 11.0,
+            ),
+            SizedBox(
+              width: 5.0,
+            ),
+            Text(
+              item,
+              style: itemUsernameContentStyle,
+            ),
+          ],
+        ),
+        Text("Price", style: itemPriceContentStyle)
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "frC",
+              style: itemBodyTextContentStyle,
+            ),
+            Text(
+              "frD",
+              style: itemBodyDateContentStyle,
+            ),
+            Text(
+              "icon-dizayn",
+              style: itemBodyTextContentStyle,
+            ),
+            Text(
+              "toC",
+              style: itemBodyTextContentStyle,
+            ),
+            Text(
+              "toD",
+              style: itemBodyDateContentStyle,
+            )
+          ],
+        ),
+        const SizedBox(height: 12),
+        Row(
+          children: <Widget>[
+            Text(
+              "Details",
+              style: itemBodyDetailContentStyle,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,49 +88,83 @@ class DetailScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: ButtonTheme(
-                      minWidth: 0,
-                      child: FlatButton(
-                        splashColor: Colors.transparent,
-                        padding: const EdgeInsets.all(0),
-                        shape: null,
-                        onPressed: () => Navigator.pop(context, null),
-                        child: const Icon(
-                          FontAwesomeIcons.times,
-                          color: secondaryColor,
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: ButtonTheme(
+                            minWidth: 0,
+                            child: FlatButton(
+                              splashColor: Colors.transparent,
+                              padding: const EdgeInsets.all(0),
+                              shape: null,
+                              onPressed: () => Navigator.pop(context, null),
+                              child: const Icon(
+                                FontAwesomeIcons.times,
+                                color: secondaryColor,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
+                    const Divider(),
+                    Text("TRAVEL DETAILS"),
+                  ],
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: <Widget>[
+                      _buildHeading(),
+                      const SizedBox(height: 12),
+                      const Divider(),
+                      const SizedBox(height: 12),
+                      _buildBody()
+                    ],
                   ),
-                  Divider(),
-                ],
+                ),
               ),
-              Row(
-                children: <Widget>[
-                  Text("Travel details"),
-                  Divider(),
-                  Text("xd")
-                ],
-              ),
-              Divider(),
-              Row(
-                children: <Widget>[Text("Package details")],
-              ),
-              Divider(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SwButton(
-                    text: 'Make a Bid',
-                    onPressed: () {
-                      print("hi");
-                    },
+              Container(
+                  child: Column(
+                children: <Widget>[const Divider(), Text("PACKAGE DETAILS")],
+              )),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: <Widget>[
+                      _buildHeading(),
+                      const SizedBox(height: 12),
+                      const Divider(),
+                      const SizedBox(height: 12),
+                      _buildBody()
+                    ],
                   ),
-                ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SwButton(
+                          text: 'MAKE A BID',
+                          onPressed: () {
+                            print("hi");
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ],
           ),
