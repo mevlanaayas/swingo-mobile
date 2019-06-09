c26e011fd5dd3b883d09ab74f32d2a1ec669655a
mevlanaayas <mevlanaayas@gmail.com>
2019-06-09 16:09:42 +0300

    implemented: bid details

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -7,6 +7,6 @@ export 'package:swingo/src/pages/welcome/about.dart';
 export 'package:swingo/src/pages/profile/profile.dart';
 export 'package:swingo/src/pages/profile/orders.dart';
 export 'package:swingo/src/pages/profile/bids.dart';
-export 'package:swingo/src/pages/profile/bid_details.dart';
+export 'package:swingo/src/pages/order_details.dart';
 export 'package:swingo/src/pages/create_orders.dart';
 export 'package:swingo/src/pages/order_list.dart';
