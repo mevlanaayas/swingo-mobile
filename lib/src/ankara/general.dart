8d0caa627c000e440756e40aa857b1a8eb06ee90
mevlanaayas <mevlanaayas@gmail.com>
2019-06-09 15:47:55 +0300

    implemented: provider to hold user status

---
+++ lib/src/ankara/general.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:swingo/src/models/models.dart';
+
+class UserStatus with ChangeNotifier {
+  bool _userLoggedIn = true;
+  User _user = null;
+
+  bool get userLoggedIn => _userLoggedIn;
+
+  User get currentUser => _user;
+
+  set userLoggedIn(bool newValue) {
+    _userLoggedIn = newValue;
+    notifyListeners();
+  }
+
+  set currentUser(User newUser) {
+    _user = newUser;
+    notifyListeners();
+  }
+}
