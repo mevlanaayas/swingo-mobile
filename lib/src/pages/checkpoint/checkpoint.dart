5db7ccd463c88b01b954c6d53af02f925f704719
mevlanaayas <mevlanaayas@gmail.com>
2019-06-18 21:37:18 +0300

    start: implementing checkpoints

---
+++ lib/src/pages/checkpoint/checkpoint.dart
@@ -0,0 +1,22 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class CheckpointScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: AspectRatio(
+          aspectRatio: 1,
+          child: Padding(
+            padding: EdgeInsets.only(top:20),
+            child: FlareActor(
+              'assets/flare/SuccessCheck.flr',
+              alignment: Alignment.center,
+              fit: BoxFit.contain,
+              animation: 'Untitled',
+            ),
+          ),
+        )
+    );
+  }
+}
