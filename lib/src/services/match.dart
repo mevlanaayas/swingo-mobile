3a8a423ef74fa037145eed3a04b9def48a5c65cc
Furkan <furkanakan@gmail.com>
2019-09-15 15:06:15 +0300

    Load next page of matches

--- lib/src/services/match.dart
+++ lib/src/services/match.dart
@@ -7,14 +7,14 @@ import 'package:swingo/src/user_status.dart';
 import 'package:swingo/src/models/match.dart';
 
 abstract class MatchService extends SwNetwork {
-  static listAll(BuildContext context,
-      {onError, onSuccess}) async {
+  static listAll(BuildContext context, {url, onError, onSuccess}) async {
     final userProvider = Provider.of<UserStatus>(context);
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
+    String requestUrl = url == null ? '$swBaseUrl/match/' : url;
     final response = await SwNetwork.sendRequest(
         context,
         () => http.get(
-              '$swBaseUrl/match/',
+              requestUrl,
               headers: {
                 "Content-type": "application/json",
                 "Authorization": 'Token ${userProvider.token}',
@@ -25,19 +25,18 @@ abstract class MatchService extends SwNetwork {
         onError: onError, onSuccess: onSuccess);
   }
 
-  static get(BuildContext context,
-      {matchId, onError, onSuccess}) async {
+  static get(BuildContext context, {matchId, onError, onSuccess}) async {
     final userProvider = Provider.of<UserStatus>(context);
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -49,14 +48,14 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.post(
-          '$swBaseUrl/match/',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-          body: SwMatch.toSenderJson(price, transporter),
-        ));
+        () => http.post(
+              '$swBaseUrl/match/',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+              body: SwMatch.toSenderJson(price, transporter),
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -68,50 +67,48 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.post(
-          '$swBaseUrl/match/',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-          body: SwMatch.toCarrierJson(transceiver),
-        ));
+        () => http.post(
+              '$swBaseUrl/match/',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+              body: SwMatch.toCarrierJson(transceiver),
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
   }
 
-  static accept(BuildContext context,
-      {matchId, onError, onSuccess}) async {
+  static accept(BuildContext context, {matchId, onError, onSuccess}) async {
     final userProvider = Provider.of<UserStatus>(context);
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/accept',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/accept',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
   }
 
-  static reject(BuildContext context,
-      {matchId, onError, onSuccess}) async {
+  static reject(BuildContext context, {matchId, onError, onSuccess}) async {
     final userProvider = Provider.of<UserStatus>(context);
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/reject',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/reject',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -123,13 +120,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/pass_payment',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/pass_payment',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -141,13 +138,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/check_box_done',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/check_box_done',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -159,13 +156,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/check_box_fail',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/check_box_fail',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -177,13 +174,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/ready_for_taking_box',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/ready_for_taking_box',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -195,13 +192,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/confirm_taking_code/?code=$code',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/confirm_taking_code/?code=$code',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -213,13 +210,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/ready_for_delivering_box',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/ready_for_delivering_box',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
@@ -231,13 +228,13 @@ abstract class MatchService extends SwNetwork {
     final String swBaseUrl = AppConfig.of(context).apiBaseUrl;
     final response = await SwNetwork.sendRequest(
         context,
-            () => http.get(
-          '$swBaseUrl/match/$matchId/confirm_delivery_code/?code=$code',
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": 'Token ${userProvider.token}',
-          },
-        ));
+        () => http.get(
+              '$swBaseUrl/match/$matchId/confirm_delivery_code/?code=$code',
+              headers: {
+                "Content-type": "application/json",
+                "Authorization": 'Token ${userProvider.token}',
+              },
+            ));
 
     SwNetwork.handleResponse(context, response,
         onError: onError, onSuccess: onSuccess);
